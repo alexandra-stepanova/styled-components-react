2b7e46abd569e05ab62703d6b1442b0eb94137f1
Sasha Stepanova <chernishevskie@yandex.ru>
2022-07-26 01:52:39 +0300

    feat: added new component Line

---
+++ src/components/Line/Line.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledLine = styled.div`
+  font-size: 1.2em;
+  color: ${(props) => props.color || "white"};
+`;
+
+export default function Line(props) {
+  return <StyledLine {...props} />;
+}
